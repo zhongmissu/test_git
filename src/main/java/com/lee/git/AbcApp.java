afca1604ec89be43fd773f9c5c89495552f8f795
zly0303 <1234@163.com>
2019-10-27 15:27:03 +0800

    修改操作

---
+++ src/main/java/com/lee/git/AbcApp.java
@@ -0,0 +1,11 @@
+/**
+ * @program: test_git
+ * @description:
+ * @author: 钟礼盈
+ * @create: 2019-10-27 15:24
+ **/
+
+package com.lee.git;
+
+public class AbcApp {
+}
--- src/main/java/com/lee/git/TestGit.java
+++ src/main/java/com/lee/git/TestGit.java
@@ -10,5 +10,6 @@ package com.lee.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("zzz");
     }
 }
