8a37f6106082c300c1cb1313079022d520b265ec
zly0303 <1234@163.com>
2019-10-27 15:22:55 +0800

    Initial commit

---
+++ src/main/java/com/lee/git/TestGit.java
@@ -0,0 +1,14 @@
+/**
+ * @program: test_git
+ * @description:
+ * @author: 钟礼盈
+ * @create: 2019-10-27 15:21
+ **/
+
+package com.lee.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
